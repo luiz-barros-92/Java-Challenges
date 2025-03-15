9e074e4441b0078c1392300878d4e910ae45c331
Luiz Barros <lck.barros@gmail.com>
2025-03-15 20:58:23 -0300

    Inicio do desafio Observer, montagem inicial das classes

---
+++ Observer/Consulta.java
@@ -0,0 +1,9 @@
+package clinicaMedica;
+
+public class Consulta {
+	private String data;
+	private String horario;
+	private String medico;
+	private String paciente;
+	
+}
---
+++ Observer/Main.java
@@ -0,0 +1,9 @@
+package clinicaMedica;
+
+public class Main {
+
+	public static void main(String[] args) {		
+
+	}
+
+}
