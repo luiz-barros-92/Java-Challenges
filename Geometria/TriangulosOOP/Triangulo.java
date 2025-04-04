436f5ec790b7ab1ae84980447dd48f6bd829c78c
Luiz Barros <lck.barros@gmail.com>
2025-04-04 19:27:18 -0300

    Resolução de desafio OOP que deve demonstrar qual dos triangulos inseridos pelo usuário é maior

---
+++ Geometria/TriangulosOOP/Triangulo.java
@@ -0,0 +1,26 @@
+package triangulo;
+
+public class Triangulo {
+	private float lado1;
+    private float lado2;
+    private float lado3;
+    
+    public Triangulo(float l1, float l2, float l3) {
+        this.lado1 = l1;
+        this.lado2 = l2;
+        this.lado3 = l3;
+    }
+    
+    public float getLado1() {
+        return lado1;
+    }
+    public float getLado2() {
+        return lado2;
+    }
+    public float getLado3() {
+        return lado3;
+    }
+    
+    
+}
+
