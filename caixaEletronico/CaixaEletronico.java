8f2ae810e4979d018f7a584698235369bedef4e5
Luiz Barros <lck.barros@gmail.com>
2025-04-05 21:56:58 -0300

    Adicionada classe 'CaixaEletrtronico' e inicio da implementação da estrutura Hash

---
+++ caixaEletronico/CaixaEletronico.java
@@ -0,0 +1,20 @@
+package caixaEletronico;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CaixaEletronico {
+    private Map<String, Cliente> clientes;
+
+    public CaixaEletronico() {
+        this.clientes = new HashMap<>();
+        
+        clientes.put("123", new Cliente("123", "senha123", 1000.0));
+        clientes.put("456", new Cliente("456", "senha456", 500.0));
+    }
+
+    public Cliente getCliente(String conta) {
+        return clientes.get(conta);
+    }
+}
+
