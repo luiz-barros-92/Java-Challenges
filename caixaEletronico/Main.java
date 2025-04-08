68b6fa69b589047391415907822c04dccffc066d
Luiz Barros <lck.barros@gmail.com>
2025-04-07 21:11:34 -0300

    Adicionado tela padrão de login

--- caixaEletronico/Main.java
+++ caixaEletronico/Main.java
@@ -1,11 +1,22 @@
 package caixaEletronico;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {		
-		Cliente c1 = new Cliente("123", "456", 1000.00);
-		System.out.println(c1.getSaldo());
-		System.out.println(c1.getSenha());
-		System.out.println(c1.getConta());
+		System.out.println("----Bem vindo ao LuizBank!----");
+		System.out.println("------Acesse sua conta:-------");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Conta: ");
+		String conta = sc.nextLine();
+		
+		System.out.print("Senha: ");
+		String senha = sc.nextLine();
+		
+		sc.close();		
+		
 	}
 }
