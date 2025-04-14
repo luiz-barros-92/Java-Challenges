03cc2dd4cd0666c465421a7bc476855be182f8ac
Luiz Barros <lck.barros@gmail.com>
2025-04-14 20:35:57 -0300

    Correção, removendo super() que entrou sem ser convidado por conta do preenchimento automático da IDE que eu não prestei atenção

--- Java-OOP/estoque/Product.java
+++ Java-OOP/estoque/Product.java
@@ -5,8 +5,7 @@ public class Product {
 	private double price;
 	private int quantity;
 	
-	public Product(String name, double price, int quantity) {
-		super();
+	public Product(String name, double price, int quantity) {		
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
