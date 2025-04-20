10d7fcf6c9088efff63db0709cbdce484b812893
Luiz Barros <lck.barros@gmail.com>
2025-04-20 18:10:56 -0300

    Correção de indentações de interação com editores de diferentes sistemas

--- Java-OOP/student/Main.java
+++ Java-OOP/student/Main.java
@@ -4,25 +4,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        
-        System.out.println("---Sistema do ColégioLB---");
-        
-        System.out.print("Nome do aluno: ");
-        String name = sc.nextLine();
-
-        System.out.print("M1: ");
-        double m1 = sc.nextDouble();
-
-        System.out.print("M2: ");
-        double m2 = sc.nextDouble();
-
-        System.out.print("M3: ");
-        double m3 = sc.nextDouble();
-
-        Student st = new Student(name, m1, m2, m3);
-
-        st.output();
-        
-        sc.close();
-    }
+	        
+	        System.out.println("---Sistema do ColégioLB---");
+	        
+	        System.out.print("Nome do aluno: ");
+	        String name = sc.nextLine();
+	
+	        System.out.print("M1: ");
+	        double m1 = sc.nextDouble();
+	
+	        System.out.print("M2: ");
+	        double m2 = sc.nextDouble();
+	
+	        System.out.print("M3: ");
+	        double m3 = sc.nextDouble();
+	
+	        Student st = new Student(name, m1, m2, m3);
+	
+	        st.output();
+	        
+	        sc.close();
+    	}
 }
