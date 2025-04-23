849dc885eb323dd961c6428eb0c6352a9872a67b
Luiz Barros <lck.barros@gmail.com>
2025-04-23 20:48:02 -0300

    Starting the Challenge 'bankAccount'

---
+++ Java-OOP/bankAccount/Main.java
@@ -0,0 +1,11 @@
+package bankAccount;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//TODO Start user scanner inputs
+		//TODO Check which path the user will choose, with or without an initial deposit
+		//TODO Manage outputs with Class/Main
+	}
+
+}
