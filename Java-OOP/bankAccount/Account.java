ba5f58bf5a0a3198ac2ebeaa34b100705b967070
Luiz Barros <lck.barros@gmail.com>
2025-04-24 20:17:58 -0300

    Java Challenge 'bankAccount' finished. It's made by watching the output from my teacher and applying reverse engineering in my code.

--- Java-OOP/bankAccount/Account.java
+++ Java-OOP/bankAccount/Account.java
@@ -1,47 +1,50 @@
-package bankAccount;
-
 public class Account {
-    private int number;
-    private String holder;
-    private double balance;    
-    
-    public Account (int number, String holder, double balance) {
-        this.number = number;
-        this.holder = holder;
-        this.balance = balance;
-    }
-    
-    public Account (int number, String holder) {
-        this.number = number;
-        this.holder = holder;
-    }
-    
-    public int getNumber() {
-        return number;
-    }
-    
-    public String getHolder() {
-        return holder;
-    }
-    
-    public void setHolder(String holder) {
-        this.holder = holder;
-    }
-    
-    public double getBalance() {
-        return balance;
-    }
-    
-    public void deposit (double value) {
-        this.balance = this.getBalance() + value;
-        System.out.println("Updated account data:");
-    }
-    
-    public void withdraw (double value) {
-        if ((this.getBalance() - 5) >= value) {
-            this.balance = (this.getBalance() - value) - 5; //bank fee $5
-        } else {
-            System.out.println("Sorry, you don't have enough balance.");
-        }
-    }
+	private int number;
+	private String holder;
+	private double balance;
+
+	public Account (int number, String holder, double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public Account (int number, String holder) {
+		this.number = number;
+		this.holder = holder;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void deposit (double value) {
+		this.balance = this.getBalance() + value;
+		System.out.println("Updated account data:");
+	}
+
+	public void withdraw (double value) {
+		if ((this.getBalance() - 5) >= value) {
+			this.balance = (this.getBalance() - value) - 5; //bank fee $5
+			System.out.println("Updated account data:");
+		} else {
+			System.out.println("Sorry, you don't have enough balance.");
+		}
+	}
+	
+	public String accountData() {
+	   return "Account: " + this.getNumber() + ", Holder: " + this.getHolder() + ", Balance: " + String.format("%.2f", this.getBalance());
+	} 
 }
--- Java-OOP/bankAccount/Main.java
+++ Java-OOP/bankAccount/Main.java
@@ -1,11 +1,49 @@
-package bankAccount;
+import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		//TODO Start user scanner inputs
-		//TODO Check which path the user will choose, with or without an initial deposit
-		//TODO Manage outputs with Class/Main
-	}
+	    Scanner sc = new Scanner(System.in);
+	    
+		System.out.print("Enter account number: ");
+		int accountNumber = sc.nextInt();
+        sc.nextLine();
+		
+		System.out.print("Enter account holder: ");
+		String accountHolder = sc.nextLine();
+		
+		System.out.print("Is there an initial deposit (y/n)? ");
+		String initialDeposit = sc.nextLine().toLowerCase();
 
+        Account ac = null;
+		
+		if (initialDeposit.equals("y")) {
+		    System.out.print("Enter initial deposit value: ");
+		    double initialValue = sc.nextDouble();
+		    ac = new Account (accountNumber, accountHolder, initialValue);
+            
+		}else if (initialDeposit.equals("n")) {
+		    ac = new Account (accountNumber, accountHolder);
+            
+		}else {
+		    System.out.println("Invalid input.");
+            return;
+		}
+        
+		System.out.println("Account data: ");
+		System.out.println(ac.accountData());
+		
+		System.out.print("Enter a deposit value: ");
+		double deposit = sc.nextDouble();
+		
+		ac.deposit(deposit);
+		System.out.println(ac.accountData());
+		
+		System.out.print("Enter a withdraw value: ");
+		double withdraw = sc.nextDouble();
+		
+		ac.withdraw(withdraw);
+		System.out.println(ac.accountData());
+
+        sc.close();
+	}
 }
