c55f8a47c4f660a448e4f1aa0a81d0adf2dffd3c
Luiz Barros <lck.barros@gmail.com>
2025-04-30 20:47:11 -0300

    Updated challenge 'EmployeeSalary'

--- Java-OOP/arrays/employeeSalary/Employee.java
+++ Java-OOP/arrays/employeeSalary/Employee.java
@@ -1,32 +1,32 @@
 package sandBox;
 
 public class Employee {
-    private Integer id;
-    private String name;
-    private Double salary;
-    
-    public Employee() {        
-    }
-    
-    public Employee(Integer id, String name, Double salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-    }
-    
-    public Integer getId(){
-        return id;
-    }
-    
-    public String getName(){
-        return name;
-    }
-    
-    public Double getSalary(){
-        return salary;
-    }
-    
-    public void increaseSalary(double percentage){
-        this.salary = this.getSalary() + (this.getSalary() * (percentage / 100));
-    }
-}
+     private Integer id;
+     private String name;
+     private Double salary;
+     
+     public Employee() {        
+     }
+     
+     public Employee(Integer id, String name, Double salary) {
+         this.id = id;
+         this.name = name;
+         this.salary = salary;
+     }
+     
+     public Integer getId(){
+         return id;
+     }
+     
+     public String getName(){
+         return name;
+     }
+     
+     public Double getSalary(){
+         return salary;
+     }
+     
+     public void increaseSalary(double percentage){
+         this.salary += this.salary * (percentage / 100);
+     }
+ }
--- Java-OOP/arrays/employeeSalary/Main.java
+++ Java-OOP/arrays/employeeSalary/Main.java
@@ -1,13 +1,49 @@
 package sandBox;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		List<Employee> list = new ArrayList<>();
 
+		System.out.print("How many employees will be registered? ");
+		int n = sc.nextInt();
+
+		for(int i=0; i<n; i++) {
+			System.out.println("Employee # " + (i+1) + ":");
+
+			System.out.print("Id: ");
+			int id = sc.nextInt();
+
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+
+			System.out.print("Salary: ");
+			double salary = sc.nextDouble();
+
+			list.add(new Employee(id, name, salary));
+		}
+		
+		System.out.print("Enter the employee id that will have salary increase: ");
+		int idIncrease = sc.nextInt();
+		
+		Employee emp = list.stream().filter(x -> x.getId() == idIncrease).findFirst().orElse(null);
+		
+		if(emp == null) {
+		    System.out.println("This id does not exist!");
+		}else {
+		    System.out.print("Enter the percentage: ");
+		    double percentage = sc.nextDouble();
+		    emp.increaseSalary(percentage);
+		}
+
+		
+		//TODO  Finish the output
 		sc.close();
-}
+	}
 }
