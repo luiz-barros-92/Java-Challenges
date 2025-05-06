c75763c3f8e668546aa3d237cde3792f6a4ac402
Luiz Barros <lck.barros@gmail.com>
2025-05-06 20:32:08 -0300

    Updated notes from LocalDateTime

--- Java-Basic/DateTime(Notes)/SeveralLocalDateTime.java
+++ Java-Basic/DateTime(Notes)/SeveralLocalDateTime.java
@@ -23,18 +23,15 @@ public class SeveralLocalDateTime {
 		Instant dt07 = Instant.parse("2025-05-04T20:48:40-03:00"); //print output: 2025-05-04T23:48:40Z
 		
 		//custom format
-        //https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
-        DateTimeFormatter ft1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter ft2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-        
-        LocalDate dt08 = LocalDate.parse("05/05/2025", ft1); //print output: 2025-05-05
-        LocalDateTime dt09 = LocalDateTime.parse("05/05/2025 20:05", ft2); //print output: 2025-05-05T20:05
-        
-        //isolated data
-        LocalDate dt10 = LocalDate.of(2025, 5, 5); //print output: 2025-05-05
-        LocalDateTime dt11 = LocalDateTime.of(2025, 5, 5, 20, 22); //print output: 2025-05-05T20:22
-        
-        
-        System.out.println(dt11);
+		//https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+	    DateTimeFormatter ft1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	    DateTimeFormatter ft2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+	    
+	    LocalDate dt08 = LocalDate.parse("05/05/2025", ft1); //print output: 2025-05-05
+	    LocalDateTime dt09 = LocalDateTime.parse("05/05/2025 20:05", ft2); //print output: 2025-05-05T20:05
+	    
+	    //isolated data
+	    LocalDate dt10 = LocalDate.of(2025, 5, 5); //print output: 2025-05-05
+	    LocalDateTime dt11 = LocalDateTime.of(2025, 5, 5, 20, 22); //print output: 2025-05-05T20:22
 	}
 }
