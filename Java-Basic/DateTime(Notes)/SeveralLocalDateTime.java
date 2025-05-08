f29f8c4a3f009491306e2f5bf21bca1a343379fe
Luiz Barros <lck.barros@gmail.com>
2025-05-07 22:15:39 -0300

    Update DateTime notes

--- Java-Basic/DateTime(Notes)/SeveralLocalDateTime.java
+++ Java-Basic/DateTime(Notes)/SeveralLocalDateTime.java
@@ -1,9 +1,8 @@
-package sandBox;
-
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.ZoneId;
 
 public class SeveralLocalDateTime {
 
@@ -26,6 +25,7 @@ public class SeveralLocalDateTime {
 		//https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
 	   	DateTimeFormatter ft1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	   	DateTimeFormatter ft2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter ft3 = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm").withZone(ZoneId.systemDefault());//captures TimeZone default from user system 
 	    
 	   	LocalDate dt08 = LocalDate.parse("05/05/2025", ft1); //print output: 2025-05-05
 	   	LocalDateTime dt09 = LocalDateTime.parse("05/05/2025 20:05", ft2); //print output: 2025-05-05T20:05
@@ -33,5 +33,13 @@ public class SeveralLocalDateTime {
 	    	//isolated data
 	    	LocalDate dt10 = LocalDate.of(2025, 5, 5); //print output: 2025-05-05
 	    	LocalDateTime dt11 = LocalDateTime.of(2025, 5, 5, 20, 22); //print output: 2025-05-05T20:22
+		
+		//applying custom format
+		System.out.println(dt04.format(ft1));//output: 04/05/2025
+		System.out.println(ft1.format(dt04));//output: 04/05/2025
+		System.out.println(dt04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));//output: 04/05/2025
+		System.out.println(dt05.format(ft2));//output: 04/05/2025 20:47
+		System.out.println(ft3.format(dt06));//output: 04/05/2025 17:48
+
 	}
 }
